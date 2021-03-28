d521db6aeabb4beda9a8779a57e87dc5787d7a07
jefferson venceslau <jeff.venceslau@gmail.com>
2021-03-28 18:55:09 -0300

    feat(index.js): check node version and export handler

---
+++ index.js
@@ -0,0 +1,14 @@
+const { node: version } = require('./package.json').engines;
+const { satisfies } = require('semver');
+
+/**
+ * Handler Function validate params
+ * Minimum node version = 10
+ */
+
+if (!satisfies(process.version, version)) {
+  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
+  process.exit(1);
+}
+
+module.exports = require('./handler');
